80474772f5b1b4b19c586d576cc5b8929076699d
Goguzgungor <goguzgungor59@gmail.com>
2023-05-04 23:39:34 +0300

    -done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import {getUrl} from './request';
 
 function App() {
+  const handleClick = async () => {
+    const url = await getUrl();
+    console.log(url); // API'den gelen URL'i konsola yazdırır
+  };
+
   return (
     <div className="App">
       <div className='app-container'>
         <h1>buromnia</h1>
-        <button onClick={""}>Generate URL</button>
+        <button onClick={handleClick}>Generate URL</button>
       </div>
     </div>
   );
